c0b671e2672cd3b6d67a460d12af6c9ebb584ffd
Hafiz Hassan <hafiz.hassan@interstellus.com>
2020-08-10 12:15:51 +0500

    Updates

---
+++ precache-manifest.76b208b56b60c389dccee4e0959f88a3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf8c6b7893935da838f6e76c270c9f35",
+    "url": "/goldeneagle/index.html"
+  },
+  {
+    "revision": "5f7240c04fd4ee910b99",
+    "url": "/goldeneagle/static/css/main.e4103276.chunk.css"
+  },
+  {
+    "revision": "ea14e2e3ab828cdf0d6d",
+    "url": "/goldeneagle/static/js/2.780a5f4c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/goldeneagle/static/js/2.780a5f4c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f7240c04fd4ee910b99",
+    "url": "/goldeneagle/static/js/main.564a3a2e.chunk.js"
+  },
+  {
+    "revision": "bff8aa8b2e7c814d4856",
+    "url": "/goldeneagle/static/js/runtime-main.bb8b84c1.js"
+  }
+]);
